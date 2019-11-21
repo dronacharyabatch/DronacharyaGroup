393589adddb07ef17a5430b83e2786ab1ec1e543
venufs <venufs@gmail.com>
2019-11-21 18:10:58 +0530

    upd

--- main.js
+++ main.js
@@ -22,8 +22,8 @@ var pool      =    mysql.createPool({
     database : '7IMrocUvYo',
     debug    :  false
 });
-function doMessage(strQuery){
-	strQuery = encodeURIData(strQuery);
+function doMessage(strQuery, callback){
+	//strQuery = encodeURIData(strQuery);
 	var req = https.request(options, (res) => {
 	  console.log('statusCode:', res.statusCode);
 	  console.log('headers:', res.headers);
@@ -38,6 +38,7 @@ function doMessage(strQuery){
 	});
 	req.write(strQuery);
 	req.end();
+	callback(200);
 }
 function formatMsg(source, args){
 	var target = '';
@@ -62,32 +63,10 @@ function executeQuery(query,callback){
             connection.release();
             if(!err) {
                 callback(null, rows);
-            }           
-        });
-        connection.on('error', function(err) {
-			console.log(err);
-              /*throw err;
-              return;   */  
-        });
-    });
-}
-function add2Db(reqData, callback){
-	console.log(reqData);
-	var isBlock = 0;
-	if("on" === reqData.is_block)
-		isBlock = 1;
-	var query = "INSERT INTO dronabatch(sname, mobile, is_block) VALUES ('"+reqData.sname+"', '"+reqData.mobile+"', "+isBlock+")";
-	pool.getConnection(function(err,connection){
-        if (err) {
-          connection.release();
-          throw err;
-        }   
-        connection.query(query,function(err,rows){
-            connection.release();
-            if(!err) {
-				callback();
-                //callback(null, rows);
-            }           
+            }else{
+				console.log(err);
+				callback(null);
+			}
         });
         connection.on('error', function(err) {
 			console.log(err);
@@ -133,22 +112,138 @@ function status(reqData){
 	console.log('statusCallback');
 	console.log(reqData);
 }
-function recieved(reqData){
-	if(reqData.Body.startsWith('add:')){
-		register(reqData);
-	}else{
-		process(reqData);
+function findBodyJson(opr, reqData){
+	reqData.Body = reqData.Body.replace(opr, '');
+	var data = reqData.Body.split(':');
+	reqData.BodyJson = {};
+	for(var idx=0; idx<data.length; idx+=2){
+		var name = data[idx];
+		var bgData = data[idx+1];
+		if(name === 'mobile')
+			bgData = 'whatsapp:+91'+bgData;
+		reqData.BodyJson[name] = bgData;
 	}
 }
-function register(reqData){
-	executeQuery("select mobile,sname,role from dronabatch where mobile='"+reqData.From+"'", (status, result)=>{
-		doMessage(getWhatsMessage({mobile:reqData.From}, "contact registered."));
+function recieved(reqData, callback){
+	if(!reqData.Body.startsWith('find:') && !reqData.Body.startsWith('add:') && !reqData.Body.startsWith('del:')){
+		processGroup(reqData, callback);
+		return;
+	}
+	checkAccess(reqData, function(status){
+		if(status === 401){
+			doMessage(getWhatsMessage({mobile:reqData.From}, "You must be Admin"), callback);
+			return;
+		}
+		if(reqData.Body.startsWith('find:')){
+			findBodyJson('find:', reqData);
+			find(reqData.BodyJson, function(result){
+				var mobile = result.mobile.replace('whatsapp:+91','');
+				doMessage(getWhatsMessage({mobile:reqData.From}, "name : "+result.name+"\nmobile : "+mobile+"\nisBlocked : "+((result.is_block === 1)?'Yes':'No')), callback);			
+			});
+		}else if(reqData.Body.startsWith('add:')){
+			var message = 'Contact Not Added.';
+			findBodyJson('add:', reqData);
+			find(reqData.BodyJson, function(result){
+				if(result){
+					remove(result, function(result1){
+						if(result1){
+							insert(reqData.BodyJson, function(result2){
+							if(result2){
+								message = formatMessage(reqData, '\nAdded.');
+							}
+							doMessage(getWhatsMessage({mobile:reqData.From}, message), callback);
+							});
+						}
+					});
+				}else{
+					insert(reqData.BodyJson, function(result1){
+						if(result1){
+							message = formatMessage(reqData, '\nAdded.');
+						}
+						doMessage(getWhatsMessage({mobile:reqData.From}, message), callback);
+					});
+				}
+			});
+		}else if(reqData.Body.startsWith('del:')){
+			var message = 'Contact Not Found';
+			findBodyJson('del:', reqData);
+			find(reqData.BodyJson, function(result){
+				if(result){
+					remove(result, function(result1){
+							if(result1){
+								message = formatMessage(reqData, "\n Removed");
+							}
+						doMessage(getWhatsMessage({mobile:reqData.From}, message), callback);			
+					});
+				}else{
+					doMessage(getWhatsMessage({mobile:reqData.From}, message), callback);			
+				}
+			});
+		}
 	});
 }
-function process(reqData){
+function formatMessage(reqData, endMsg){
+	var message = '';
+	if(reqData.BodyJson.name)
+		message += "name : "+reqData.BodyJson.name;
+	if(reqData.BodyJson.mobile){
+		var mobile = reqData.BodyJson.mobile.replace('whatsapp:+91','');
+		message += "\nmobile : "+mobile;
+	}
+	message += endMsg;
+	return message;
+}
+function insert(reqData, callback){
+	if(!reqData.is_block)
+		reqData.is_block = "0";
+	if(!reqData.role)
+		reqData.role = "0";
+	var query = "insert into dronateam(name, mobile, is_block, role) values('"+reqData.name+"', '"+reqData.mobile+"', "+reqData.is_block+", "+reqData.role+")";
+	executeQuery(query, (status, resultData)=>{
+		callback(resultData);
+	});
+}
+function remove(reqData, callback){
+	var query = "delete from dronateam where mobile='"+reqData.mobile+"'";
+	executeQuery(query, (status, resultData)=>{
+		callback(resultData);
+	});
+}
+function checkAccess(reqData, callback){
+	var query = "select role,is_block from dronateam where mobile='"+reqData.From+"'";
+	executeQuery(query, (status, resultData)=>{
+		var result = resultData[0];
+		if(result.role === 0){
+			callback(401);
+		}else{
+			callback(200);
+		}
+	});
+}
+function find(reqData, callback){
+	var isAnd = false;
+	var query = "select mobile,name,role,is_block from dronateam where ";
+	if(reqData.mobile){
+		query +="mobile='"+reqData.mobile+"'";
+		isAnd = true;
+	}
+	if(reqData.name){
+		if(isAnd)
+			query +=" AND ";
+		query +="name='"+reqData.name+"'";
+	}
+	executeQuery(query, (status, resultData)=>{
+		var result;
+		if(resultData.length > 0)
+			result = resultData[0];
+		callback(result);
+	});
+}
+
+function processGroup(reqData, callback){
 	var group = [];
 	var sender;
-	executeQuery('select * from dronabatch', (status, result)=>{
+	executeQuery('select * from dronateam', (status, result)=>{
 		result.forEach((item, index) =>{
 			if(item.is_block !== 0)
 				return;
@@ -160,7 +255,7 @@ function process(reqData){
 			
 		});
 		console.log(sender);
-		var message = formatMsg(TEMPLATE, [ sender.sname, reqData.Body ]);
+		var message = formatMsg(TEMPLATE, [ sender.name, reqData.Body ]);
 		var MediaContentType0 = reqData["MediaContentType0"];
 		var mediaUris = [];
 		if (MediaContentType0 != null && MediaContentType0.trim().length > 0) {
@@ -174,10 +269,11 @@ function process(reqData){
 			if (mediaUris.length > 0) {
 				item['MediaUrl'] = mediaUris;
 			}
-			doMessage(getWhatsMessage(item, message));
+			doMessage(getWhatsMessage(item, message), callback);
 		});
 	});
 }
+
 http.createServer(function (req, res) {
 	var fMsg = "Message sent\n";
 	var body = [];
@@ -189,16 +285,21 @@ http.createServer(function (req, res) {
 			var bodyMsg = Buffer.concat(body).toString();
 			var reqData = QueryStringToJSON(decodeURIComponent(bodyMsg));
 			if('/recieved' === req.url){
-				recieved(reqData);
+				recieved(reqData, (status)=> {
+					res.writeHead(status);
+					res.end(fMsg);
+				});
 			}else if('/status' === req.url){
-				status(reqData);
+				status(reqData, ()=> {
+					res.writeHead(200);
+					res.end(fMsg);
+				});
 			}
 			console.log(fMsg);
-		  res.writeHead(200);
-		  res.end(fMsg);
+		  
 		});
 	}else{
 		res.end('Requested resource Not available.');
-		res.writeHead(200);
+		res.writeHead(404);
 	}
 }).listen(8080);
